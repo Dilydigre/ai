f01d1701a7a27c0743dce7ba784fe815f2de2b6f
acmo0 <72703330+acmo0@users.noreply.github.com>
2024-05-31 19:29:52 +0200

    Change random image generation

--- app/models/UNET_model.py
+++ app/models/UNET_model.py
@@ -7,7 +7,7 @@ from .UNET import UNet
 class UNETModel:
 	
 	def __init__(self, PATH):
-		self.gaussian_noise = GaussianNoise(1)
+		self.gaussian_noise = GaussianNoise(25)
 		self.unet = UNet()
 		self.model_PATH=PATH
 		self.unet.load_state_dict(torch.load(self.model_PATH)['model_state_dict']) #load le fichier de la VM de save/modele1 dans le UNET
@@ -15,9 +15,7 @@ class UNETModel:
 # On crée ensuite les deux fonctions nécessaires à la liaison entre l'AI et l'API 
 
 	def generate_without_prompt(self):
-		#tensor 3 512 512 avec tous les coefficients à 0 
-		input_tensor = torch.zeros(1,3,512,512)
-		noise_image = self.gaussian_noise(input_tensor) #image bruitée
+		noise_image = torch.tensor(np.random.rand(1, 3, 512, 512).astype('float32')) #image bruitée
 		unet_image = self.unet(noise_image) #image bruitée passée à travers le U-Net
 		resized_image = torch.reshape(unet_image, (512, 512, 3)) # on reshape l'image avec torch.reshape
 		normalized_image = torch.clamp(resized_image, 0, 1)*255 # on normalise avec torch.clamp pour avoir des valeurs entre 0 et 255
